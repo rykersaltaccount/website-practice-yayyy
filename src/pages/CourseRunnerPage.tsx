7ad87b032ae8e286e7b817bd3fe583777de15a20
Ryker <adrianitogonzalinio@gmail.com>
2026-08-28 07:29:23 -0300

    Storage commit

--- src/pages/CourseRunnerPage.tsx
+++ src/pages/CourseRunnerPage.tsx
@@ -107,6 +107,7 @@ const CourseRunnerPage: React.FC = () => {
   const [generationError, setGenerationError] = useState('');
   const generatedLessonKey = useRef<string | null>(null);
   const drillEditorRef = useRef<HTMLDivElement>(null);
+    const [showNextLessonPopup, setShowNextLessonPopup] = useState(false);
   const drills = [...(lesson?.drills || []), ...(lesson?.capstone ? [lesson.capstone] : [])];
   const activeDrill: Drill | undefined = drills[drillIndex];
 
